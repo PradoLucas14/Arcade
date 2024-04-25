f280ee75878268e3e555b0c01de2c1409623b585
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-04-25 20:23:53 -0300

    se creo la funcionalidad para destacar juegos

--- frontend/js/administrador.js
+++ frontend/js/administrador.js
@@ -59,7 +59,57 @@ async function getJuegos() {
     }
 }
 
-function mostrarJuegosEnTabla(juegos) {
+async function obtenerJuegosDestacados() {
+    const url = 'http://localhost:3000/destacados';
+
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error al obtener juegos destacados:', error);
+        return [];
+    }
+}
+
+async function mostrarJuegosEnTabla(juegos) {
+    const tablaJuegos = document.getElementById("tablaJuegos");
+    tablaJuegos.innerHTML = "";    
+
+    const juegosDestacados = await obtenerJuegosDestacados();
+
+    juegos.forEach(juego => {
+        const iconoTrue = '<i class="bi bi-check-square-fill"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="green" class="bi bi-check-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708"/></svg></i>';
+        const iconoFalse = '<i class="bi bi-x-square-fill"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#DE0202" class="bi bi-x-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708"/></svg></i>';
+
+        const esDestacado = juegosDestacados.some(destacado => destacado.titulo === juego.titulo);
+        const iconoPublicado = juego.publicado ? iconoTrue : iconoFalse;
+        const claseDestacado = esDestacado ? 'destacado' : '';
+
+        const fila = `
+            <tr class="fila ${claseDestacado}">
+                <td class="text-center">${juego.id}</td>
+                <td>${juego.titulo}</td>
+                <td>${juego.genero}</td>
+                <td>${juego.descripcion}</td>
+                <td class="text-center">${iconoPublicado}</td>
+                <td class="funciones">
+                    <button class="btn btn-sm editar" onclick="editarJuego(${juego.id})"><i class="bi bi-pencil-square"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                    </svg></i></button>
+                    <button class="btn btn-sm eliminar" onclick="eliminarJuego(${juego.id})"><i class="bi bi-trash3-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                    </svg></i></button>
+                    <button class="btn btn-sm destacar" onclick="destacarJuego(${juego.id})"><i class="bi bi-star-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                  </svg></i></button>
+                </td>
+            </tr>`;
+        tablaJuegos.innerHTML += fila;
+    });
+}
+
+/*function mostrarJuegosEnTabla(juegos) {
     const tablaJuegos = document.getElementById("tablaJuegos");
     tablaJuegos.innerHTML = "";    
     tablaJuegos.innerHTML
@@ -86,14 +136,14 @@ function mostrarJuegosEnTabla(juegos) {
                     <button class="btn btn-sm eliminar" onclick="eliminarJuego(${juego.id})"><i class="bi bi-trash3-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                     </svg></i></button>
-                    <button class="btn btn-sm destacar" onclick="DestacarJuego(${juego.id})"><i class="bi bi-star-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <button class="btn btn-sm destacar" onclick="destacarJuego(${juego.id})"><i class="bi bi-star-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-star-fill" viewBox="0 0 16 16">
                     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                   </svg></i></button>
                 </td>
             </tr>`;
         tablaJuegos.innerHTML += fila;
     });
-}
+}/*
 
 /*function mostrarJuegosEnTabla(juegos) {
     const tablaJuegos = document.getElementById("tablaJuegos");
@@ -211,6 +261,44 @@ async function editarJuego(id) {
         });
     } catch (error) {
         console.error("Error al obtener el juego para editar:", error);
+    }
+}
+
+async function obtenerDatosJuego(id) {
+    const url = `http://localhost:3000/juegos/${id}`;
+
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error al obtener datos del juego:', error);
+        return null;
+    }
+}
+
+async function destacarJuego(id) {
+    const datosJuego = await obtenerDatosJuego(id);
+
+    if (datosJuego) {
+        const url = 'http://localhost:3000/destacados';
+        const juegoSeleccionado = {
+            id: datosJuego.id,
+            titulo: datosJuego.titulo,
+            desarrolladora: datosJuego.desarrollador,
+            imagen2: datosJuego.imagen2
+        };
+
+        try {
+            const response = await axios.post(url, juegoSeleccionado);
+            console.log('Juego destacado exitosamente:', response.data);
+            alert("Se agrego este juego a destacados")
+            return
+        } catch (error) {
+            console.error('Error al destacar el juego:', error);
+            alert("No se pudo agregar este juego a destacados")
+        }
+    } else {
+        console.error('No se pudieron obtener los datos del juego.');
     }
 }
 
