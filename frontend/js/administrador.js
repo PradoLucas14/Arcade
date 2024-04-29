e231f9efdfe770aeda052346f5340a561324af2c
tobofear <tobifedearias@gmail.com>
2024-04-28 23:13:44 -0300

    linkeo pagina de error y mas cosas

--- frontend/js/administrador.js
+++ frontend/js/administrador.js
@@ -107,14 +107,14 @@ function mostrarJuegosEnTabla(juegos) {
                 <td class="descripcion-celda">${juego.descripcion}</td>
                 <td class="text-center">${iconoPublicado}</td>
                 <td class = "funciones">
-                    <button class="btn btn-sm editar" onclick="editarJuego(${juego.id})"><i class="bi- bi-pencil-square"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <button class="btn btn-sm editar" onclick="editarJuego('${juego.id}')"><i class="bi- bi-pencil-square"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                     </svg></i></button>
-                    <button class="btn btn-sm eliminar" onclick="eliminarJuego(${juego.id})"><i class="bi- bi-trash3-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <button class="btn btn-sm eliminar" onclick="eliminarJuego('${juego.id}')"><i class="bi- bi-trash3-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                     </svg></i></button>
-                    <button class="btn btn-sm destacar" onclick="destacarJuego(${juego.id})"><i class="bi bi-star-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <button class="btn btn-sm destacar" onclick="destacarJuego('${juego.id}')"><i class="bi bi-star-fill"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#fff" class="bi bi-star-fill" viewBox="0 0 16 16">
                     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                   </svg></i></button>
                 </td>
@@ -285,7 +285,9 @@ async function obtenerDatosJuego(id) {
 
     try {
         const response = await axios.get(url);
+        console.log(response.data)
         return response.data;
+        
     } catch (error) {
         console.error('Error al obtener datos del juego:', error);
         return null;
@@ -298,20 +300,28 @@ async function destacarJuego(id) {
 
     if (datosJuego) {
         // URL del servidor JSON para actualizar los datos del juego destacado con el ID 1
-        const url = 'http://localhost:3000/destacados/';
+        const url = 'http://localhost:3000/destacado/';
+        console.log(url);
+        const response = await axios.get(url);
+        console.log(response);
+        const primerDestacado = response.data[0];
+        console.log(primerDestacado);
 
         // Datos del juego seleccionado
         const juegoSeleccionado = {
+            id: datosJuego.id,
             titulo: datosJuego.titulo,
-            desarrolladora: datosJuego.desarrolladora,
+            desarrollador: datosJuego.desarrollador,
             anio: datosJuego.anio,
             genero: datosJuego.genero,
-            imagen2: datosJuego.imagen2,
+            imagen1: datosJuego.imagen1,
         };
 
         try {
+            console.log(datosJuego.id);
+            console.log(juegoSeleccionado.id);
             // Realizar la solicitud PATCH al servidor JSON utilizando Axios y esperar la respuesta
-            const response = await axios.patch(url[0], juegoSeleccionado);
+            const response = await axios.patch(`${url}${primerDestacado.id}`, juegoSeleccionado);
             console.log('Juego destacado actualizado exitosamente:', response.data);
             Swal.fire({
                 icon: 'success',
@@ -422,6 +432,7 @@ async function cerrarSesion(event){
 
             console.log("Usuario deslogueado:", usuarioLogueado.nombre);
             console.log("Estado de logueo actual:", usuarioLogueado.logueado);
+            window.location.href= '../index.html';
         } else {
             console.log("No hay ningún usuario logueado.");
         }
